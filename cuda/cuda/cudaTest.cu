6a12b8198565e456f0836ed4e5e46401859a45e3
FlorianTh2 <s0558101@htw-berlin.de>
2019-12-18 10:00:57 +0100

    Added basic functionalities

--- cuda/cuda/cudaTest.cu
+++ cuda/cuda/cudaTest.cu
@@ -14,12 +14,20 @@ __global__ void test()
 	return;
 }
 
+// rgb->Ycbcr
 __global__ void dev_convertColorSpace(unsigned char* dev_data, unsigned char* dev_dataResult, int dataSize)
 {
-
-
-	*dev_dataResult = 1;
-
+	int offset = 0;
+	int channels = 3;
+	for(int dataElement = 0; dataElement < (dataSize*channels); dataElement+channels)
+	{
+		unsigned char* r = (dev_dataResult + offset+0)
+		unsigned char* g = *(dev_dataResult + offset+1)
+		unsigned char* b = *(dev_dataResult + offset+2)
+		*r = 16+ (((*r << 6) + (*r << 1) + (*g << 7) + *g + (*b << 4) + (*b << 3) + *b) >> 8); // Y
+		*g= 128 + ((-((*r<<5)+(*r<<2)+(*r<<1))-((*g<<6)+(*g<<3)+(*g<<1))+(*b<<7)-(*b<<4))>>8);; // Cb
+		*b = 128 + (((*r<<7)-(*r<<4)-((*g<<6)+(*g<<5)-(*g<<1))-((*b<<4)+(*b<<1)))>>8);; // Cr
+	}
 }
 
 __global__ void dev_applyGaussian(unsigned char* dev_data, unsigned char* dev_dataResult, int dataSize)
@@ -27,6 +35,25 @@ __global__ void dev_applyGaussian(unsigned char* dev_data, unsigned char* dev_da
 
 
 	*dev_dataResult = 1;
+
+
+
+    double stdv = 1.0;
+    double r, s = 2.0 * stdv * stdv;  // Assigning standard deviation to 1.0
+    double sum = 0.0;   // Initialization of sun for normalization
+    for (int x = -2; x <= 2; x++) // Loop to generate 5x5 kernel
+    {
+        for(int y = -2; y <= 2; y++)
+        {
+            r = sqrt(x*x + y*y);
+            gk[x + 2][y + 2] = (exp(-(r*r)/s))/(M_PI * s);
+            sum += gk[x + 2][y + 2];
+        }
+    }
+
+    for(int i = 0; i < 5; ++i) // Loop to normalize the kernel
+        for(int j = 0; j < 5; ++j)
+            gk[i][j] /= sum;
 
 }
 
